c879283df45140081f3151f222240cfce4a92234
xbol0 <xbolo@duck.com>
2023-03-18 16:27:36 +0800

    add image proxy

--- src/index.ts
+++ src/index.ts
@@ -32,19 +32,56 @@ const Uploaders = [
   nostrImgUpload,
 ]
 
+const AllowHosts = [
+  "nostr.build",
+  "void.cat",
+  "member.cash",
+  "pomf2.lain.la",
+  "i.nostrimg.com",
+]
+
 export default {
   async fetch(req: Request): Promise<Response> {
-    if (req.method !== "POST" && req.method !== "PUT") {
-      return new Response(null, { status: 404 })
+    if (req.method === "POST") {
+      return await handleUpload(req)
     }
 
-    const filename = req.headers.get("x-filename") || new URL(req.url).pathname.slice(1) || null
-    const type = req.headers.get("x-mime-type")
+    if (req.method == "PUT") {
+      return await handleUpload(req)
+    }
 
-    const blob = await req.blob()
-    const fn = Uploaders[Math.floor(Uploaders.length * Math.random())]
+    if (req.method === "GET") {
+      return await handleGetImage(req)
+    }
 
-    const str = await fn(blob, filename, type)
-    return new Response(str)
+    return new Response(null, { status: 404 })
   },
 };
+
+async function handleUpload(req: Request) {
+  const filename = req.headers.get("x-filename") || new URL(req.url).pathname.slice(1) || null
+  const type = req.headers.get("x-mime-type")
+
+  const blob = await req.blob()
+  const fn = Uploaders[Math.floor(Uploaders.length * Math.random())]
+
+  const str = await fn(blob, filename, type)
+
+  const u = new URL(str)
+  const cur = new URL(req.url)
+  const target = new URL(u.host + u.pathname, cur.origin)
+
+  return new Response(target.href)
+}
+
+async function handleGetImage(req: Request) {
+  const url = new URL(req.url)
+  const paramstr = url.pathname.slice(1)
+  const [host, ...paths] = paramstr.split("/")
+
+  if (!AllowHosts.includes(host)) {
+    return new Response(null, { status: 404 })
+  }
+  const imgUrl = new URL(paths.join("/"), `https://${host}`)
+  return fetch(imgUrl)
+}
